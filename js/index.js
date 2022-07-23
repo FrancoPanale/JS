bab3cf4044bca5c611e3c998fbd79a1628adaf51
Franco Pañale <francopanale@gmail.com>
2022-07-23 16:23:24 -0300

    Desafio Complementario

---
+++ js/index.js
@@ -0,0 +1,14 @@
+debugger
+
+let nombre = prompt("Ingresa tu Nombre")
+nombre = nombre.toLocaleLowerCase()
+
+if (true){
+console.log("Bienvenido al Desafio Complementario" + "" + nombre)
+}
+
+let limite = parseInt(prompt("Ingrese su clave"))
+
+for (let i = limite; i <= 30; i++){
+    console.log("Clave Numero:" , i)
+}
